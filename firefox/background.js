b5a6f8c1db36da50a9f5d6d9e1d3082e19ead64d
Joe Herbert <joe@joeherbert.dev>
2024-03-16 10:05:27 +0000

    Add updated page

--- firefox/background.js
+++ firefox/background.js
@@ -16,3 +16,25 @@ function handleMessage(request, sender, sendResponse) {
 }
 
 browser.runtime.onMessage.addListener(handleMessage);
+
+browser.runtime.onInstalled.addListener(async ({
+    reason,
+    temporary
+}) => {
+    if (temporary) return;
+    var url;
+    switch (reason) {
+        case "install":
+            await browser.tabs.create({
+                url: "https://joeherbert.dev/sideloads/?onboard=true&browser=firefox"
+            });
+            break;
+        case "update":
+            await browser.tabs.create({
+                url: browser.runtime.getURL("updated.html")
+            });
+            break;
+    }
+});
+
+browser.runtime.setUninstallURL("https://joeherbert.dev/sideloads/uninstall");
